cc48f27b4e30a049818e4715376068c2ef6eba37
Hari <augten95@gmail.com>
2019-09-13 16:08:34 -0500

    updated photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendMatch = [
     {
         name: "James",
-        photo: "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppies-15.jpg",
+        photo: "http://cdn.akc.org/content/article-body-image/siberian_husky_cute_puppies.jpg",
         scores: ['1', '5', '5', '1', '5', '1', '2', '3', '4', '5']
     }, {
         name: "Wyatt",
@@ -13,23 +13,23 @@ var friendMatch = [
         scores: ['2', '4', '1', '1', '1', '1', '1', '2', '1', '5']
     }, {
         name: "Shawn Mendes",
-        photo: "https://www.google.com/search?q=shawn+mendes&safe=strict&rlz=1C1EJFA_enUS711US711&sxsrf=ACYBGNRw9CFXedhxOnyaCd3bouZD37Ilpg:1568406354525&tbm=isch&source=iu&ictx=1&fir=iH6iBSX7wVXP1M%253A%252CiuthFPxP9nrgyM%252C%252Fm%252F010rf07y&vet=1&usg=AI4_-kRP7d7bFrM7ENu0Ccuph7SElFY58w&sa=X&ved=2ahUKEwjSu__80M7kAhWU9Z4KHVswDw0Q_B0wJ3oECAEQAw#imgrc=iH6iBSX7wVXP1M:",
+        photo: "https://en.wikipedia.org/wiki/File:Shawn_Mendes_at_The_Queen%27s_Birthday_Party_(cropped_2).jpg",
         scores: ['4', '5', '4', '5', '4', '5', '4', '5', '4', '5']
     }, {
         name: "Ellen DeGeneres",
-        photo: "https://www.google.com/search?q=ellen+degeneres&safe=strict&rlz=1C1EJFA_enUS711US711&sxsrf=ACYBGNQtDDEHACEiWjXGUNVP28cV2ATD2Q:1568406514017&tbm=isch&source=iu&ictx=1&fir=HbzSxdZWzc7V4M%253A%252CiTArEj09nlwAQM%252C%252Fm%252F01gbbz&vet=1&usg=AI4_-kTwRMaPceLO4AiE0c9uKWWFkzj4QA&sa=X&ved=2ahUKEwiHi4bJ0c7kAhXC854KHfINB6wQ_B0wLHoECAEQAw#imgrc=HbzSxdZWzc7V4M:",
+        photo: "https://media2.s-nbcnews.com/j/newscms/2018_50/1394339/ellen-degeneres-portia-today-inline-181212_8c47fc7ec8f0a2a931f1d2d7b2e3aada.fit-760w.jpg",
         scores: ['5', '5', '5', '5', '5', '5', '5', '5', '5', '5']
     }, {
         name: "Kelly Clarkson",
-        photo: "https://www.google.com/search?q=Kelly+Clarkson&safe=strict&rlz=1C1EJFA_enUS711US711&sxsrf=ACYBGNQbI3p4CtJDrpl0JYU5-kl4_ZGlwg:1568406540423&tbm=isch&source=iu&ictx=1&fir=-BOFB_YYsOM67M%253A%252C4WiOoHhuwvrUXM%252C%252Fm%252F025ldg&vet=1&usg=AI4_-kQRaPtu2D-E9zyhd1JBOnsfRFEwvg&sa=X&ved=2ahUKEwij39HV0c7kAhUS7J4KHR6DA9cQ_B0wKnoECAEQAw#imgrc=-BOFB_YYsOM67M:",
+        photo: "https://parade.com/wp-content/uploads/2019/09/kelly-clarkson-american-idol-bitter.jpg",
         scores: ['4', '4', '4', '4', '4', '4', '4', '4', '4', '4']
     }, {
         name: "Serena Williams",
-        photo: "https://www.google.com/search?q=Serena+Williams&safe=strict&rlz=1C1EJFA_enUS711US711&sxsrf=ACYBGNQfpTgDLMrDErUcw7iKN01J5xhw2g:1568406561867&tbm=isch&source=iu&ictx=1&fir=7qpFjmw2TGOKcM%253A%252CCLKZCRkgJJqtAM%252C%252Fm%252F015z4j&vet=1&usg=AI4_-kTGHPY0MV11D8ixkvjeqQlw8EeSkA&sa=X&ved=2ahUKEwiMyO7f0c7kAhWD_J4KHb4CBmkQ_B0wKXoECAAQAw#imgrc=7qpFjmw2TGOKcM:",
+        photo: "https://peopledotcom.files.wordpress.com/2019/09/serena-williams-2.jpg",
         scores: ['5', '4', '4', '2', '4', '4', '3', '2', '3', '3']
     }, {
         name: "Camila Cabello",
-        photo: "https://www.google.com/search?q=Camila+Cabello&safe=strict&sa=X&rlz=1C1EJFA_enUS711US711&stick=H4sIAAAAAAAAAONgFuLWz9U3MDQoSjMwr1TiAnHKqtJzSkq0-AJSi4rz84IzU1LLEyuLF7HyOSfmZuYkKjgnJqXm5OTvYGUEANksoNQ_AAAA&sxsrf=ACYBGNRHUD3NNCmNXk8oq4B80KdJxP-6Ig:1568406379504&tbm=isch&source=iu&ictx=1&fir=5_M9UDiuV9G03M%253A%252CwVrUM25W_4yFsM%252C%252Fm%252F0vzgltt&vet=1&usg=AI4_-kQQzGagbx-rDgbGFxc3h3twTjyh2A&ved=2ahUKEwjRkPSI0c7kAhVCnJ4KHT4cB8cQ_B0wJ3oECAEQAw#imgrc=5_M9UDiuV9G03M:",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/d/de/Camila_Cabello_VMA_2018.jpg",
         scores: []
     }, {
         name: "Kermit the Frog",
